2391c3f2bdae419e42c07e28b751368ee0f4f4d6
Roshan Kumar <roshan@Roshans-MacBook-Air.local>
2025-05-27 11:16:10 +0530

    update Selection

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor }) => {
+  const [bgColor, setBgColor] = useState("");
+
+  const handleClick = () => {
+    applyColor(setBgColor); // applyColor will pass the selected color to setBgColor
+  };
+
+  return (
+    <div
+      className="fix-box"
+      onClick={handleClick}
+      style={{ background: bgColor }}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+
+export default Selection;
